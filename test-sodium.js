40ef225daccad67e704e83564238e4fd9ecbebc7
Badruduja04 <badruduja652@gmail.com>
2026-07-08 04:04:27 +0700

    Remove temporary and development files

--- test-sodium.js
+++
@@ -1,4 +0,0 @@
-import sodium from 'sodium-native';
-console.log('sodium-native imported successfully!');
-import voice from '@discordjs/voice';
-console.log('@discordjs/voice imported successfully!');
